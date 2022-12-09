0d0b3b1279b7da00f81792e4bc78ff24cf0596b9
danutdotrar <morarasu.danut@gmail.com>
2022-12-10 00:55:45 +0200

    Add basic functions

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
+const inputDisplay = document.querySelector('.display--screen');
+
+const numberBtns = document.querySelectorAll('.number');
+
+const operators = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal-sign');
+
+
+// Populate display when click number
+numberBtns.forEach(el=> el.addEventListener('click', displayNumber));
+
+function displayNumber() {
+    inputDisplay.value += this.value;
+}
+
 
 // Functions
-function add(a, b) {
-    console.log(a + b)
-}
+const add = (a, b) => a + b;
 
-function substract(a, b) {
-    console.log(a - b)
-}
+const substract = (a, b) => a - b;
 
-function multiply(a, b) {
-    console.log(a * b);
-}
+const multiply = (a, b) => a * b;
 
-function divide(a, b) {
-    console.log(a / b)
-}
-
-function operate(operator, a, b) {
-    add(a,b)
-}
-
-
-
+const divide = (a, b) => a / b;
