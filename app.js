5db4d60e888bd5264f52638f8edfc8a82f1eac41
danutdotrar <morarasu.danut@gmail.com>
2022-12-09 23:47:56 +0200

    Add calculator buttons

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('miau')
+
+// Functions
+function add(a, b) {
+    console.log(a + b)
+}
+
+function substract(a, b) {
+    console.log(a - b)
+}
+
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+function divide(a, b) {
+    console.log(a / b)
+}
+
+function operate(operator, a, b) {
+    add(a,b)
+}
+
+
+
