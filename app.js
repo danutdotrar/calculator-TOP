50e91f6e46b231c7d916be65e3bfc666c4d11086
danutdotrar <morarasu.danut@gmail.com>
2022-12-10 12:45:57 +0200

    add operate function

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ const equalBtn = document.querySelector('.equal-sign');
 // Populate display when click number
 numberBtns.forEach(el=> el.addEventListener('click', displayNumber));
 
-function displayNumber() {
-    inputDisplay.value += this.value;
-}
 
+// Create the functions that populate the display when you click the number buttons. 
+// You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 
 // Functions
 const add = (a, b) => a + b;
@@ -22,3 +21,29 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+// Create a new function operate that takes an operator and 2 numbers 
+// and then calls one of the above functions on the numbers.
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+
+    switch(operator) {
+        case '+':
+            return add(a, b)
+            break;
+        case '-':
+            return substract(a, b)
+            break;
+        case '*':
+            return multiply(a, b)
+            break;
+        case '/':
+            if (b === 0) return "Can't divide by zero";
+            return divide(a, b)
+            break;
+    }
+}
+
+
+console.log(operate('+', 2, 3));
